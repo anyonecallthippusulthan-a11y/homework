ddf5135524720b11a039ef7e126ccb42a94125ad
anyonecallthippusulthan-a11y <anyonecallthippusulthan@gmail.com>
2026-03-02 11:41:01 +0530

    operator learn

---
+++ hw/Relational.java
@@ -0,0 +1,22 @@
+class Relational 
+{
+	public static void main(String[] args)
+	{
+          byte a=10;
+          byte b=20;
+         Boolean hi= a==b;
+         Boolean h= a>=b;
+         Boolean i= a<=b;
+         Boolean c= a!=b;
+         Boolean o= a>b;
+         Boolean t= a<b;
+       System.out.println("equal to  value                 :" +hi);
+        System.out.println(" less than equal to  value     :" +h);
+         System.out.println("greater than equal to  value  :" +i);
+           System.out.println("not equal to  value         :" +c);
+             System.out.println("less than  value          :" +o);
+               System.out.println("grater than   value     :" +t);
+	} 
+}
+       
+         
---
+++ hw/arith.java
@@ -0,0 +1,17 @@
+class arith
+{
+	public static void main(String[] args)
+	{
+	  byte a=20;
+          byte b=10;
+        int c=a+b;
+        int d  = a-5;
+        int e  = a*b;
+        int f  =a/2;
+       System.out.println("the additional value"  +c); 
+       System.out.println("the subtraction value" +d);
+       System.out.println("the multi value"       +e);
+       System.out.println("the divison value"     +f);
+        
+         }
+}
