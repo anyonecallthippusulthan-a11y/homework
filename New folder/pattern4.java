5fa0c42e5ea530e18e54a8b4e19309070fd63cec
anyonecallthippusulthan-a11y <anyonecallthippusulthan@gmail.com>
2026-03-12 06:59:48 +0530

    pattern

---
+++ New folder/pattern4.java
@@ -0,0 +1,19 @@
+class pattern3
+{
+	public static void main(String[] args)
+	{
+		
+		for ( int i=1;i<=5;i++)
+		{
+			char h='A';
+			for( int j=1;j<=i;j++)
+			{
+				
+				
+				System.out.print(h++);
+			}
+			System.out.println();
+			
+		}
+	}
+}
